4d0b5e322effdf7b659d78610d5010fc9db3234e
medovarszki <mihaly@medovarszki.hu>
2022-06-06 12:08:17 +0200

    fixing API description

--- src/main/java/hu/auxin/ibkrfacade/configuration/OpenApiConfiguration.java
+++ src/main/java/hu/auxin/ibkrfacade/configuration/OpenApiConfiguration.java
@@ -1,6 +1,7 @@
 package hu.auxin.ibkrfacade.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +11,13 @@ public class OpenApiConfiguration {
 
     @Bean
     public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("SpringShop API")
-                        .description("Spring shop sample application"));
+        return new OpenAPI().info(new Info()
+                .title("IBKR Facade")
+                .description("Web endpoints for accessing Interactive Brokers TWS API")
+                .contact(new Contact()
+                        .name("Mihaly Medovarszki")
+                        .email("mihaly@medovarszki.hu")
+                        .url("https://github.com/medovarszki/ibkrfacade")
+                ));
     }
 }
